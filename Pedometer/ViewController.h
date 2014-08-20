e270eddd49f1ef31338b1dd382dc2b7e48341e45
Andrew Erickson <aerickson14@gmail.com>
2014-08-19 19:16:49 -0500

    Initial Commit

---
+++ Pedometer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pedometer
+//
+//  Created by Andrew Erickson on 2014-08-19.
+//  Copyright (c) 2014 Andrew Erickson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pedometer/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Pedometer
+//
+//  Created by Andrew Erickson on 2014-08-19.
+//  Copyright (c) 2014 Andrew Erickson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
